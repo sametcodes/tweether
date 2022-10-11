869a9def2949684efbb3a4484ad62a9383b340a7
Samet <hey@samet.codes>
2022-10-11 06:23:02 +0300

    changes network id and name

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -2,7 +2,7 @@
 import ABI from './Tweether.json';
 
 const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS || "0x0";
-const CONTRACT_NETWORK_ID = 3;
-const CONTRACT_NETWORK_NAME = "ropsten";
+const CONTRACT_NETWORK_ID = 5;
+const CONTRACT_NETWORK_NAME = "goerli";
 
 export { ABI, CONTRACT_ADDRESS, CONTRACT_NETWORK_ID, CONTRACT_NETWORK_NAME }
